c17a400d4693100bfcfed65a8ee6b8d332e378fb
scalespeeder <66777640+scalespeeder@users.noreply.github.com>
2021-01-05 19:58:08 +0000

    Add files via upload

---
+++ start-comm-online-tools.bat
@@ -0,0 +1,26 @@
+@echo off
+:start
+::Server name
+set serverName=Your Server Name Here
+::Server files location
+set serverLocation="C:\Program Files (x86)\Steam\steamapps\common\DayZServerChernarus"
+::Server Port
+set serverPort=2302
+::Server config
+set serverConfig=serverDZ.cfg
+::Logical CPU cores to use (Equal or less than available)
+set serverCPU=2
+::Sets title for terminal (DONT edit)
+title %serverName% batch
+::DayZServer location (DONT edit)
+cd "%serverLocation%"
+echo (%time%) %serverName% started.
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-profiles=config" "-mod=@CF;@Community-Online-Tools" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+::Time in seconds before kill server process (14400 = 4 hours)
+timeout 14390
+taskkill /im DayZServer_x64.exe /F
+::Time in seconds to wait before..
+timeout 10
+::Go back to the top and repeat the whole cycle again
+goto start
